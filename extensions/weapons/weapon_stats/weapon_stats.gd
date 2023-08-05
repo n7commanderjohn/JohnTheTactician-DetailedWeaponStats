f03995b147793d1c75fc936107c901fa9dbd05e0
Johnna Morrison <johnna.morrison@rapidscale.net>
2023-08-05 18:32:07 -0400

    improved crit details added cooldown details

--- extensions/weapons/weapon_stats/weapon_stats.gd
+++ extensions/weapons/weapon_stats/weapon_stats.gd
@@ -19,12 +19,51 @@ func _ready():
 #func _process(delta):
 #	pass
 
+func get_text(base_stats:Resource)->String:
+	var text = ""
+	
+	text += Text.text("DAMAGE_FORMATTED", [col_a + tr("STAT_DAMAGE") + col_b, get_damage_text(base_stats)])
+	
+	var critString = "\n" + Text.text("CRITICAL_FORMATTED", [col_a + tr("CRITICAL") + col_b, get_crit_damage_text(base_stats), get_crit_chance_text(base_stats)])  + col_b
+
+	# ModLoaderLog.info("final crit string", critString)
+	text += critString
+
+	text += "\n" + Text.text("STAT_FORMATTED", [col_a + tr("COOLDOWN") + col_b, get_cooldown_text(base_stats)])
+
+	
+	text += get_knockback_text(base_stats)
+	text += get_range_text(base_stats)
+	text += get_bouncing_text(base_stats)
+	text += get_piercing_text(base_stats)
+	text += get_lifesteal_text(base_stats)
+	text += get_additional_cooldown_text(base_stats)
+	text += get_deal_dmg_on_return_text()
+	text += get_alternate_attack_type_text()
+	
+	return text
+
 func get_crit_chance_text(base_stats:Resource)->String:
-	var original = .get_crit_chance_text(base_stats)
-	# var a = get_col_a(crit_chance, base_stats.crit_chance)
-	# return a + str(max(crit_chance * 100.0, 0)) + col_b
+	# var original = .get_crit_chance_text(base_stats)
+	var a = get_col_a(crit_chance, base_stats.crit_chance)
+	var original = a + str(max(crit_chance * 100.0, 0)) + col_b
 	
 	var modified = original if crit_chance == base_stats.crit_chance else original+"%)" + init_a + "(" + str(max(base_stats.crit_chance * 100.0, 0))
-	ModLoaderLog.info("modded crit string", modified)
+
+	# ModLoaderLog.info("original crit string", original)
+	# ModLoaderLog.info("modded crit string", modified)
 	
+	return modified
+
+func get_cooldown_text(base_stats:Resource, multiplier:float = 1.0)->String:
+	var original = .get_cooldown_text(base_stats, multiplier)
+
+	var cd = get_cooldown_value(base_stats)
+	var base_cd = get_base_cooldown_value(base_stats)
+	var a = get_col_a( - cd, - base_cd)
+	# return a + str(stepify(cd * multiplier, 0.01)) + "s" + col_b
+
+	var modified = original if cd == base_cd else a + str(stepify(cd * multiplier, 0.01)) + "s" + col_b + init_a + str(stepify(base_cd * multiplier, 0.01)) + "s"  + col_b
+	# ModLoaderLog.info("modded cooldown string", modified)
+
 	return modified
