36a6d863bc8d8ed93c2c5781b54e4edfcddb4087
Johnna Morrison <johnna.morrison@rapidscale.net>
2023-08-12 20:44:01 -0400

    extend two and install

--- extensions/singletons/menu_data.gd
+++ extensions/singletons/menu_data.gd
@@ -4,5 +4,6 @@ func _init():
     var modloader = ModLoaderMod.new()
     var logger = ModLoaderLog.new()
     logger.info("installing now", "weapon details mod")
-    modloader.install_script_extension("res://mods-unpacked/JohnTheTactician-DetailedWeaponStats/extensions/weapons/weapon_stats/weapon_stats.gd")
+    modloader.install_script_extension("res://mods-unpacked/JohnTheTactician-DetailedWeaponStats/extensions/weapons/weapon_stats/ranged_weapon_stats.gd")
+    modloader.install_script_extension("res://mods-unpacked/JohnTheTactician-DetailedWeaponStats/extensions/weapons/weapon_stats/melee_weapon_stats.gd")
     logger.info("installing finished", "weapon details mod")
--- extensions/weapons/weapon_stats/weapon_stats.gd
+++ extensions/weapons/weapon_stats/weapon_stats.gd
@@ -1,69 +1,69 @@
-extends "res://weapons/weapon_stats/weapon_stats.gd"
+# extends "res://weapons/weapon_stats/weapon_stats.gd"
 
-# Brief overview of what the changes in this file do...
-# Expands weapon stats
+# # Brief overview of what the changes in this file do...
+# # Expands weapon stats
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# # Declare member variables here. Examples:
+# # var a = 2
+# # var b = "text"
 
-const MYMOD_LOG = "JohnTheTactician-DetailedWeaponStats" # ! Change `MODNAME` to your actual mod's name
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	ModLoaderLog.info("Ready for weapon detail stats", MYMOD_LOG)
+# const MYMOD_LOG = "JohnTheTactician-DetailedWeaponStats" # ! Change `MODNAME` to your actual mod's name
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# # Called when the node enters the scene tree for the first time.
+# func _ready():
+# 	ModLoaderLog.info("Ready for weapon detail stats", MYMOD_LOG)
 
-func get_text(base_stats:Resource)->String:
-	var text = ""
+
+# # Called every frame. 'delta' is the elapsed time since the previous frame.
+# #func _process(delta):
+# #	pass
+
+# func get_text(base_stats:Resource)->String:
+# 	var text = ""
 	
-	text += Text.text("DAMAGE_FORMATTED", [col_a + tr("STAT_DAMAGE") + col_b, get_damage_text(base_stats)])
+# 	text += Text.text("DAMAGE_FORMATTED", [col_a + tr("STAT_DAMAGE") + col_b, get_damage_text(base_stats)])
 	
-	var critString = "\n" + Text.text("CRITICAL_FORMATTED", [col_a + tr("CRITICAL") + col_b, get_crit_damage_text(base_stats), get_crit_chance_text(base_stats)])  + col_b
+# 	var critString = "\n" + Text.text("CRITICAL_FORMATTED", [col_a + tr("CRITICAL") + col_b, get_crit_damage_text(base_stats), get_crit_chance_text(base_stats)])  + col_b
 
-	# ModLoaderLog.info("final crit string", critString)
-	text += critString
+# 	# ModLoaderLog.info("final crit string", critString)
+# 	text += critString
 
-	text += "\n" + Text.text("STAT_FORMATTED", [col_a + tr("COOLDOWN") + col_b, get_cooldown_text(base_stats)])
+# 	text += "\n" + Text.text("STAT_FORMATTED", [col_a + tr("COOLDOWN") + col_b, get_cooldown_text(base_stats)])
 
 	
-	text += get_knockback_text(base_stats)
-	text += get_range_text(base_stats)
-	text += get_bouncing_text(base_stats)
-	text += get_piercing_text(base_stats)
-	text += get_lifesteal_text(base_stats)
-	text += get_additional_cooldown_text(base_stats)
-	text += get_deal_dmg_on_return_text()
-	text += get_alternate_attack_type_text()
+# 	text += get_knockback_text(base_stats)
+# 	text += get_range_text(base_stats)
+# 	text += get_bouncing_text(base_stats)
+# 	text += get_piercing_text(base_stats)
+# 	text += get_lifesteal_text(base_stats)
+# 	text += get_additional_cooldown_text(base_stats)
+# 	text += get_deal_dmg_on_return_text()
+# 	text += get_alternate_attack_type_text()
 	
-	return text
+# 	return text
 
-func get_crit_chance_text(base_stats:Resource)->String:
-	# var original = .get_crit_chance_text(base_stats)
-	var a = get_col_a(crit_chance, base_stats.crit_chance)
-	var original = a + str(max(crit_chance * 100.0, 0)) + col_b
+# func get_crit_chance_text(base_stats:Resource)->String:
+# 	# var original = .get_crit_chance_text(base_stats)
+# 	var a = get_col_a(crit_chance, base_stats.crit_chance)
+# 	var original = a + str(max(crit_chance * 100.0, 0)) + col_b
 	
-	var modified = original if crit_chance == base_stats.crit_chance else original+"%)" + init_a + "(" + str(max(base_stats.crit_chance * 100.0, 0))
+# 	var modified = original if crit_chance == base_stats.crit_chance else original+"%)" + init_a + "(" + str(max(base_stats.crit_chance * 100.0, 0))
 
-	# ModLoaderLog.info("original crit string", original)
-	# ModLoaderLog.info("modded crit string", modified)
+# 	# ModLoaderLog.info("original crit string", original)
+# 	# ModLoaderLog.info("modded crit string", modified)
 	
-	return modified
+# 	return modified
 
-func get_cooldown_text(base_stats:Resource, multiplier:float = 1.0)->String:
-	var original = .get_cooldown_text(base_stats, multiplier)
+# func get_cooldown_text(base_stats:Resource, multiplier:float = 1.0)->String:
+# 	var original = .get_cooldown_text(base_stats, multiplier)
 
-	var cd = get_cooldown_value(base_stats)
-	var base_cd = get_base_cooldown_value(base_stats)
-	var a = get_col_a( - cd, - base_cd)
-	# return a + str(stepify(cd * multiplier, 0.01)) + "s" + col_b
+# 	var cd = get_cooldown_value(base_stats)
+# 	var base_cd = get_base_cooldown_value(base_stats)
+# 	var a = get_col_a( - cd, - base_cd)
+# 	# return a + str(stepify(cd * multiplier, 0.01)) + "s" + col_b
 
-	var modified = original if cd == base_cd else a + str(stepify(cd * multiplier, 0.01)) + "s" + col_b + init_a + str(stepify(base_cd * multiplier, 0.01)) + "s"  + col_b
-	# ModLoaderLog.info("modded cooldown string", modified)
+# 	var modified = original if cd == base_cd else a + str(stepify(cd * multiplier, 0.01)) + "s" + col_b + init_a + str(stepify(base_cd * multiplier, 0.01)) + "s"  + col_b
+# 	# ModLoaderLog.info("modded cooldown string", modified)
 
-	return modified
+# 	return modified
